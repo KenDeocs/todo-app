5160b97477e9c27d434c7bdfa370a1fbddfc7490
KenDeocs <ken.deocs@gmail.com>
2023-04-10 18:25:11 +0800

    styling the user interface

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,56 @@
         <title>Todo App</title>
 
    <link rel="stylesheet" href="{{ asset('bootstrap-5.0.2-dist/css/bootstrap.min.css') }}">
+   <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     </head>
     <body>
       
+<section class="main d-flex align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+<div class="card-header">
+    <h1 class="display-6">Todo's</h1>
+</div>
+<div class="card-body">
+    <div class="row">
+        <div class="col-md-12">
+        <form action="" method="POST">
+        @csrf
+            <div class="input-group">
+                
+                  
+                <input type="text" name="" class="form-control" id="" placeholder="Task">
+                <button class="btn btn-primary">Add</button>
+
+            </div>
+            </form>
+        </div>
+        <div class="col-md-12 mt-3">
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+        <th>Task</th>
+        <th>Action</th>
+    </tr>
+                </thead>
+<tbody>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+</tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<div class="card-footer">&copy; todo's app</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <script src="{{ asset('bootstrap-5.0.2-dist/js/bootstrap.min.js') }}"></script>
     </body>
